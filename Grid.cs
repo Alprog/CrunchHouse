cd06292d88500d95ae245b512dee87119e9ce8f2
Alexander Tuzhik <alprog@hotmail.com>
2023-09-09 21:00:34 +0200

    MeshBuilder

--- Grid.cs
+++ Grid.cs
@@ -16,50 +16,20 @@ public partial class Grid : Node3D
 
 	public Mesh CreateMesh()
 	{
-		var arrayMesh = new ArrayMesh();
+		var builder = new MeshBuilder();
 
-		var vertices = new Vector3[]
+		for (int x = 0; x < 200; x++)
 		{
-			new Vector3(0, 0, 0),
-			new Vector3(1, 0, 0),
-			new Vector3(0, 0, 1),
-			new Vector3(1, 0, 1)			
-		};
+			for (int z = 0; z < 200; z++)
+			{
+				builder.AddTile(x, z);
+			}
+		}
+	
 
-		var uv = new Vector2[]
-		{
-			new Vector2(0, 0),
-			new Vector2(1, 0),
-			new Vector2(0, 1),
-			new Vector2(1, 1)			
-		};
-
-		var indecies = new int[]
-		{
-			0, 1, 2, 
-			2, 1, 3
-		};
-
-		var normals = new Vector3[]
-		{
-			Vector3.Up,
-			Vector3.Up,
-			Vector3.Up,
-			Vector3.Up
-		};
-
-		var arrays = new Godot.Collections.Array();
-		arrays.Resize((int)Mesh.ArrayType.Max);
-		arrays[(int)Mesh.ArrayType.Vertex] = vertices;
-		arrays[(int)Mesh.ArrayType.TexUV] = uv;
-		arrays[(int)Mesh.ArrayType.Index] = indecies;
-		arrays[(int)Mesh.ArrayType.Normal] = normals;
-
-		arrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, arrays);
-		
 		var material = GD.Load("res://grid.material") as Material;
-		arrayMesh.SurfaceSetMaterial(0, material);
+		builder.Materials.Add(material);
 
-		return arrayMesh;
+		return builder.Build();
 	}
 }
