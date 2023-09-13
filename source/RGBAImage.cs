0c6e2ccc60548e67286c51eabc50114448e085e5
Alexander Tuzhik <alprog@hotmail.com>
2023-09-14 00:17:38 +0200

    blit

--- source/RGBAImage.cs
+++ source/RGBAImage.cs
@@ -1,11 +1,15 @@
 
 
+using System;
 using Godot;
 
 public class RGBAImage
 {
     private Vector2I Size;
     private RGBA[,] Data;
+
+    public int Width => Size.X;
+    public int Height => Size.Y;
 
     public RGBAImage(Vector2I size)
     {
@@ -36,8 +40,23 @@ public class RGBAImage
         }
     }
 
-    public void Blit(RGBAImage srcImage, Rect2I srcRect, Vector2I pos)
+    private int GetOffset(Vector2I pos) => pos.Y * Width + pos.X; // row-major
+
+    public void Blit(RGBAImage srcImage, Rect2I srcRect, Vector2I dstPos)
     {
-        
+        Blit(srcImage, srcRect.Position, this, dstPos, srcRect.Size);
+    }
+
+    static public void Blit(RGBAImage srcImage, Vector2I srcPos, RGBAImage dstImage, Vector2I dstPos, Vector2I size)
+    {
+        var lineWidth = size.X;
+        var srcIndex = srcImage.GetOffset(srcPos);
+        var dstIndex = dstImage.GetOffset(dstPos);
+        for (int line = 0; line < size.Y; line++)
+        {
+            Array.Copy(srcImage.Data, srcIndex, dstImage.Data, dstIndex, lineWidth);
+            srcIndex += srcImage.Width;
+            dstIndex += dstImage.Width;
+        }        
     }
 }
