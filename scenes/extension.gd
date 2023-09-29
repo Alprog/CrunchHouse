a2a5c7f6685833e4afc3f876ca53b4c6afe6614a
Alexander Tuzhik <alprog@hotmail.com>
2023-09-30 01:49:01 +0200

    GDScript

---
+++ scenes/extension.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var i:int = 3
+	i = i + 1
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
